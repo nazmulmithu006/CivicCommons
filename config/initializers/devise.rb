846c68bed5ea9580c8a5d804a9e11353880c5cf6
Parama Danoesubroto <paramaw@gmail.com>
2011-03-01 19:02:05 -0500

    Added "email" access right on omniauth facebook authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -178,7 +178,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, Civiccommons::Config.facebook_app_id, Civiccommons::Config.facebook_app_secret
+  config.omniauth :facebook, 
+    Civiccommons::Config.facebook_app_id, 
+    Civiccommons::Config.facebook_app_secret, 
+    :scope => 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
