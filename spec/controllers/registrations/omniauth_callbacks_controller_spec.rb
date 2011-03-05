1ff92afe425c0e777402347aa43a59ed297f31f2
Parama Danoesubroto <paramaw@gmail.com>
2011-03-04 21:06:42 -0500

    refactor and add more specs on omniauth callback controller.

--- spec/controllers/registrations/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/registrations/omniauth_callbacks_controller_spec.rb
@@ -22,29 +22,85 @@ describe Registrations::OmniauthCallbacksController, "handle facebook authentica
       def given_a_registered_user
         @person = Factory.create(:registered_user)
       end
-      it "should successfully link the account to facebook" do
-        stub_successful_auth
-        given_a_registered_user
-        @person.authentications.count.should == 0
-        sign_in @person
-        get :facebook
-        @person.facebook_authenticated?.should be_true
-        @person.authentications.count.should == 1
-        flash[:notice].should == "Successfully linked your accaunt to Facebook"
+      
+      context "when successfully authenticated to facebook" do
+        before(:each) do
+          stub_successful_auth
+          given_a_registered_user
+          sign_in @person
+          get :facebook
+        end
+        
+        it "should have linked the account with facebook" do
+          @person.facebook_authenticated?.should be_true
+        end
+        
+        it "should redirect to homepage" do
+          response.should redirect_to root_path
+        end
+        
+        it "should display the success message" do
+          flash[:notice].should == "Successfully linked your accaunt to Facebook"
+        end
+        
       end
-      it "should not link the account to facebook when failed authentication" do
-        stub_failed_auth
-        given_a_registered_user
-        @person.authentications.count.should == 0
-        sign_in @person
-        get :facebook
-        @person.facebook_authenticated?.should be_false
-        @person.authentications.count.should == 0
-        flash[:notice].should == "Could not link your accaunt to Facebook"
+      context "when failed authenticating to facebook" do
+        before(:each) do
+          stub_failed_auth
+          given_a_registered_user
+          sign_in @person
+          get :facebook
+        end
+        
+        it "should not have link the account with facebook" do
+          @person.facebook_authenticated?.should be_false
+        end
+        
+        it "should redirect to homegape" do
+          response.should redirect_to root_path
+        end
+        
+        it "should display failed to link message" do
+          flash[:notice].should == "Could not link your accaunt to Facebook"
+        end
+        
       end
     end
     context "Not logged in and logging in using facebook" do
-      pending
+      
+      def mock_authentication(stubs={})
+        @mock_authentication ||= mock_model(Authentication, stubs).as_null_object
+      end
+      
+      def mock_person(stubs={})
+        @mock_person ||= mock_model(Person, stubs).as_null_object
+      end
+      
+      def given_a_registered_user_w_facebook_auth
+        @person = Factory.create(:registered_user)
+        @authentication = Factory.build(:authentication)
+        @person.facebook_authentication = @authentication
+      end
+      
+      context "and successfully logging in using facebook" do
+        before(:each) do
+          stub_successful_auth
+          given_a_registered_user_w_facebook_auth
+          @controller.stub(:signed_in?).and_return(false)
+          Authentication.should_receive(:find_from_auth_hash).and_return(@authentication)
+          sign_in @person
+          get :facebook
+        end
+      
+        it "should redirect to homepage" do
+          response.should redirect_to root_path
+        end
+        
+        it "should display successful login using facebook" do  
+          flash[:notice].should == 'Successfully authorized from Facebook account.'
+        end
+        
+      end
     end
   end
 
