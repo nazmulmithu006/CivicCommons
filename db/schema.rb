c58bed700e864daaa95762efb62d85faf8e6b04c
Parama Danoesubroto <paramaw@gmail.com>
2011-05-05 15:58:28 -0400

    Added max_selected_options to surveys

--- db/schema.rb
+++ db/schema.rb
@@ -362,6 +362,7 @@ ActiveRecord::Schema.define(:version => 20110607183415) do
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "max_selected_options", :default => 0
   end
 
   create_table "top_items", :force => true do |t|
